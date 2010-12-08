d4568ab2b39df8139dac4099079e394d9dd6f0cc
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2010-12-08 11:17:17 +0100

    bug fix in primitive CylindricalShell

--- matlab/AddCylindricalShell.m
+++ matlab/AddCylindricalShell.m
@@ -9,13 +9,13 @@ cylinder.ATTRIBUTE.Priority = prio;
 cylinder.ATTRIBUTE.Radius = rad; 
 cylinder.ATTRIBUTE.ShellWidth = shell_width; 
 
-cylinder.P0.ATTRIBUTE.X=start(1);
-cylinder.P0.ATTRIBUTE.Y=start(2);
-cylinder.P0.ATTRIBUTE.Z=start(3);
+cylinder.P1.ATTRIBUTE.X=start(1);
+cylinder.P1.ATTRIBUTE.Y=start(2);
+cylinder.P1.ATTRIBUTE.Z=start(3);
 
-cylinder.P1.ATTRIBUTE.X=stop(1);
-cylinder.P1.ATTRIBUTE.Y=stop(2);
-cylinder.P1.ATTRIBUTE.Z=stop(3);
+cylinder.P2.ATTRIBUTE.X=stop(1);
+cylinder.P2.ATTRIBUTE.Y=stop(2);
+cylinder.P2.ATTRIBUTE.Z=stop(3);
 
 cylinder = AddPrimitiveArgs(cylinder,varargin{:});
 
