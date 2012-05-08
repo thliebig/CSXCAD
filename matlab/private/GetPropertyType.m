42c6eb226dc273b61f294807ab146b3be2180a17
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2012-05-08 14:15:19 +0200

    octave compatibility fix in GetPropertyType

--- matlab/private/GetPropertyType.m
+++ matlab/private/GetPropertyType.m
@@ -4,7 +4,7 @@ function type_name = GetPropertyType(CSX,name)
 % internal function to get the type of a given property
 
 type_name = '';
-prop_types = fields(CSX.Properties);
+prop_types = fieldnames(CSX.Properties);
 for n=1:numel(prop_types)
     for p = 1:numel(CSX.Properties.(prop_types{n}))
         if (strcmp(CSX.Properties.(prop_types{n}){p}.ATTRIBUTE.Name,name))
