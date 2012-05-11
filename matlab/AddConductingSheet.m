3b88f8c94f392c5a9a96908d9c4ed062bba2c4ac
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2012-05-11 10:51:59 +0200

    matlab: Conducting Sheet: fallback to PEC + value checks

--- matlab/AddConductingSheet.m
+++ matlab/AddConductingSheet.m
@@ -5,6 +5,9 @@ function CSX = AddConductingSheet(CSX, name, conductivity, thickness)
 % Remember to add at least one or more 2D!! geometrical primitives to this
 % property.
 %
+% Hint:
+%   Set the thickness to 0 to fall back to a perfect metal (AddMetal)
+%
 %   example:
 %       % create the conducting material peroperty, e.g. 40um thick copper
 %       CSX = AddConductingSheet(CSX,'copper',56e6,40e-6);
@@ -16,5 +19,27 @@ function CSX = AddConductingSheet(CSX, name, conductivity, thickness)
 % CSXCAD matlab interface
 % -----------------------
 % author: Thorsten Liebig 2012
+
+if ((conductivity<0) || (thickness<0))
+    error('CSXCAD:AddConductingSheet','a negative conductivity or thickness is invalid');
+end
+
+if (conductivity<1e6)
+    warning('CSXCAD:AddConductingSheet','a conductivity below 1MA/Vm is not recommended');
+end
+
+if (thickness>500e-6)
+    warning('CSXCAD:AddConductingSheet','a thickness greater than 500um is not recommended');
+end
+
+% fall back to ideal pec for t==0
+if (thickness==0)
+    CSX = AddMetal(CSX,name);
+    return;
+end
+
+if (thickness<1e-6)
+    warning('CSXCAD:AddConductingSheet','a thickness lower than 1um is not recommended');
+end
 
 CSX = AddProperty(CSX, 'ConductingSheet', name,'Conductivity',conductivity,'Thickness',thickness);
