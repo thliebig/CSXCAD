1bd07e324fdffb6963bee7dbe8f40a1485026b09
Sebastian Held <sebastian@buero.fritz.box>
2012-07-13 20:39:24 +0200

    help update

--- matlab/SetMaterialProperty.m
+++ matlab/SetMaterialProperty.m
@@ -2,17 +2,17 @@ function CSX = SetMaterialProperty(CSX, name, varargin)
 % function CSX = SetMaterialProperty(CSX, name, varargin)
 %
 % Use this function to define the material constants:
-%   'Epsilon':  relative electric permitivity
-%   'Mue':      relative magnetic permeability
-%   'Kappa':    electric conductivity
-%   'Sigma':    magnetc conductivity (non-physical property)
+%   'Epsilon':  relative electric permittivity: [Epsilon] = 1
+%   'Mue':      relative magnetic permeability: [Mue} = 1
+%   'Kappa':    electric conductivity: [Kappa] = S/m
+%   'Sigma':    magnetic conductivity (non-physical property): [Sigma] = Ohm/m
 %
 % examples:
 % CSX = AddMaterial( CSX, 'RO3010' );
 % CSX = SetMaterialProperty( CSX, 'RO3010', 'Epsilon', 10.2, 'Mue', 1 );
 % CSX = AddBox( CSX, 'RO3010', 0, [0 0 0], [100 1000 1000] );
 %
-% %unisotropic material
+% % anisotropic material
 % CSX = AddMaterial( CSX, 'sheet','Isotropy',0);
 % CSX = SetMaterialProperty(CSX, 'sheet', 'Kappa', [0 0 kappa]);
 % CSX = AddBox( CSX, 'sheet', 0, [0 0 0], [10 1000 1000] );
