4e27541be9d512d796d00b58d28046d95e718ad5
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2012-09-27 14:16:38 +0200

    matlab: doc update for AddLorentzMaterial
    Signed-off-by: Thorsten Liebig <Thorsten.Liebig@gmx.de>

--- matlab/AddLorentzMaterial.m
+++ matlab/AddLorentzMaterial.m
@@ -5,17 +5,27 @@ function CSX = AddLorentzMaterial(CSX, name, varargin)
 % Note: openEMS currently only supports a drude material type.
 %
 % The drude type frequency dependent material:
-% eps_r(w) = eps_r* ( 1 - w_plasma^2/(w*(w-j/t_r)) )
-% mue_r(w) = mue_r* ( 1 - w_plasma^2/(w*(w-j/t_r)) )
+% eps_r(w) = eps_r* ( 1 - w_eps_plasma^2/(w*(w-j/t_eps_r)) )
+% mue_r(w) = mue_r* ( 1 - w_mue_plasma^2/(w*(w-j/t_mue_r)) )
 % with
-% w_plasma: the respective plasma frequency
-% t_r:      the respective relaxation time
+% w_eps_plasma: the respective electric angular plasma frequency
+% w_mue_plasma: the respective magnetic angular plasma frequency
+% t_eps_r:      the respective electric relaxation time
+% t_mue_r:      the respective magnetic relaxation time
 %
 % Use SetMaterialProperty to define the material constants:
-%   'EpsilonPlasmaFrequency':  electric plasma frequency
-%   'MuePlasmaFrequency':      magnetic plasma frequency
+%   'EpsilonPlasmaFrequency':  electric plasma frequency (f_eps_plasma)
+%   'MuePlasmaFrequency':      magnetic plasma frequency (f_mue_plasma)
 %   'EpsilonRelaxTime':        electric plasma relaxation time (losses)
 %   'MueRelaxTime':            magnetic plasma relaxation time (losses)
+%
+% Note: all properties must be positive values
+%
+% Higher order Drude type:
+%   'EpsilonPlasmaFrequency_<n>':  n-th order electric plasma frequency (f_eps_plasma)
+%   'MuePlasmaFrequency_<n>':      n-th order magnetic plasma frequency (f_mue_plasma)
+%   'EpsilonRelaxTime_<n>':        n-th order electric plasma relaxation time (losses)
+%   'MueRelaxTime_<n>':            n-th order magnetic plasma relaxation time (losses)
 %
 % example:
 %     CSX = AddLorentzMaterial(CSX,'drude');
