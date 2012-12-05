f1a660df78b9c3f403a516220dfa6fcd857f764f
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2012-12-05 15:20:43 +0100

    doc: polyhedron reader doc updates
    Signed-off-by: Thorsten Liebig <Thorsten.Liebig@gmx.de>

--- matlab/ImportPLY.m
+++ matlab/ImportPLY.m
@@ -1,9 +1,14 @@
 function CSX = ImportPLY(CSX, propName, prio, filename, varargin)
 % function CSX = ImportPLY(CSX, propName, prio, filename, varargin)
 %
+% example:
+%   CSX = AddMetal( CSX, 'cad_model' ); % create a perfect electric conductor (PEC)
+%   CSX = ImportPLY(CSX, 'cad_model',10, 'sphere.ply','Transform',{'Scale',1/unit});
+%
+%   Note: make sure the file 'sphere.ply' is in the working directory
 %
 % See also AddBox, AddCylinder, AddCylindricalShell, AddSphere, AddSphericalShell,
-% AddCurve, AddWire, AddMetal
+% AddCurve, AddWire, AddMetal, ImportSTL
 %
 % CSXCAD matlab interface
 % -----------------------
--- matlab/ImportSTL.m
+++ matlab/ImportSTL.m
@@ -1,9 +1,14 @@
 function CSX = ImportSTL(CSX, propName, prio, filename, varargin)
 % function CSX = ImportSTL(CSX, propName, prio, filename, varargin)
 %
+% example:
+%   CSX = AddMetal( CSX, 'cad_model' ); % create a perfect electric conductor (PEC)
+%   CSX = ImportSTL(CSX, 'cad_model',10, 'sphere.stl','Transform',{'Scale',1/unit});
+%
+%   Note: make sure the file 'sphere.stl' is in the working directory
 %
 % See also AddBox, AddCylinder, AddCylindricalShell, AddSphere, AddSphericalShell,
-% AddCurve, AddWire, AddMetal
+% AddCurve, AddWire, AddMetal, ImportPLY
 %
 % CSXCAD matlab interface
 % -----------------------
