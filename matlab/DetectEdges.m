280146d2a910037e4e7b3f686f9f64add31eb691
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2013-04-26 09:56:55 +0200

    new advanced option for DetectEdges
    Signed-off-by: Thorsten Liebig <Thorsten.Liebig@gmx.de>

--- matlab/DetectEdges.m
+++ matlab/DetectEdges.m
@@ -14,6 +14,9 @@ function mesh = DetectEdges(CSX, mesh, varargin)
 %   'Set_Property'  set the list of properties to detect (override default)
 %                   e.g. 'Metal' or {'Metal','ConductingSheet'}
 %
+% advanced options: 'keyword', value
+%   '2D_Metal_Edge_Res' define a one-third/two-third metal edge resolution
+%
 % example:
 %     CSX = InitCSX();
 %     % define all properties and primitives to detect
@@ -31,7 +34,7 @@ function mesh = DetectEdges(CSX, mesh, varargin)
 % CSXCAD matlab interface
 % -----------------------
 % author: Koen De Vleeschauwer (c) 2012
-% modified by: Thorsten Liebig (c) 2012
+% modified by: Thorsten Liebig (c) 2012, 2013
 %
 % See also InitCSX, SmoothMesh, DefineRectGrid
 
@@ -43,12 +46,12 @@ supported_properties{end+1}='LumpedElement';
 supported_properties{end+1}='ConductingSheet';
 
 debug = 1;
+mesh_2D_metal_edges = 0;
 
 for n=1:2:numel(varargin)
     if (strcmpi(varargin{n},'debug')==1);
         debug = varargin{n+1};
-    end
-    if (strcmpi(varargin{n},'add_property')==1);
+    elseif (strcmpi(varargin{n},'add_property')==1);
         if iscell(varargin{n+1})
             supported_properties(end+1) = varargin{n+1};
         elseif ischar(varargin{n+1})
@@ -56,8 +59,7 @@ for n=1:2:numel(varargin)
         else
            error('CSXCAD:DetectEdges','unknown property definition');
         end
-    end
-    if (strcmpi(varargin{n},'set_property')==1);
+    elseif (strcmpi(varargin{n},'set_property')==1);
         if iscell(varargin{n+1})
             supported_properties = varargin{n+1};
         elseif ischar(varargin{n+1})
@@ -65,6 +67,8 @@ for n=1:2:numel(varargin)
         else
            error('CSXCAD:DetectEdges','unknown property definition');
         end
+    elseif (strcmpi(varargin{n},'2D_Metal_Edge_Res')==1);
+        mesh_2D_metal_edges = varargin{n+1}*[-1 2]/3;
     end
 end
 
@@ -103,6 +107,7 @@ if (isfield(CSX, 'Properties'))
         if (sum(strcmpi(prop_fn{p}, supported_properties))==0)
             continue;
         end
+        isMetal = sum(strcmpi(prop_fn{p},{'Metal','ConductingSheet'}));
         property_group = CSX.Properties.(prop_fn{p});
         for m = 1:numel(property_group)
             property=property_group{m};
@@ -121,9 +126,20 @@ if (isfield(CSX, 'Properties'))
                         x2 = box.P2.ATTRIBUTE.X;
                         y2 = box.P2.ATTRIBUTE.Y;
                         z2 = box.P2.ATTRIBUTE.Z;
-                        % add to global list of edges
-                        edges = AddEdge (edges, box, x1, y1, z1, CoordSystem, debug);
-                        edges = AddEdge (edges, box, x2, y2, z2, CoordSystem, debug);
+                        % check dimension
+                        dim = (x1~=x2) + (y1~=y2) + (z1~=z2);
+                        if ((dim==2) && isMetal)
+                            % add to global list of edges, with a given 2D
+                            % edge resolution
+                            edges = AddEdge (edges, box, x1+sign(x1-x2)*mesh_2D_metal_edges, y1+sign(y1-y2)*mesh_2D_metal_edges, ...
+                                z1+sign(z1-z2)*mesh_2D_metal_edges, CoordSystem, debug);
+                            edges = AddEdge (edges, box, x2+sign(x2-x1)*mesh_2D_metal_edges, y2+sign(y2-y1)*mesh_2D_metal_edges, ...
+                                z2+sign(z2-z1)*mesh_2D_metal_edges, CoordSystem, debug);
+                        else
+                            % add to global list of edges
+                            edges = AddEdge (edges, box, x1, y1, z1, CoordSystem, debug);
+                            edges = AddEdge (edges, box, x2, y2, z2, CoordSystem, debug);
+                        end
                     end
                 elseif (strcmp(prim_fn{n_prim}, 'LinPoly') || strcmp(prim_fn{n_prim}, 'Polygon'))
                     for l = 1:length(primitives.(prim_fn{n_prim}))
@@ -197,13 +213,12 @@ end
 end
 
 
-
 function edges = AddEdge(edges, csx_prim, x, y, z, CoordSystem, debug)
 % Add edges of CSX primitives including some transformations
 
-xt = x;
-yt = y;
-zt = z;
+xt = unique(x);
+yt = unique(y);
+zt = unique(z);
 
 if isfield(csx_prim.ATTRIBUTE,'CoordSystem')
     if (csx_prim.ATTRIBUTE.CoordSystem~=CoordSystem)
@@ -238,6 +253,5 @@ end
 edges.x = [edges.x xt];
 edges.y = [edges.y yt];
 edges.z = [edges.z zt];
-
 end
 
