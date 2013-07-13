2c324e5e2fa2d7812e9a97045f72701db5d6ff65
Thorsten Liebig <Thorsten.Liebig@gmx.de>
2013-07-13 13:10:16 +0200

    fix in mesh symmetry check function
    Signed-off-by: Thorsten Liebig <Thorsten.Liebig@gmx.de>

--- matlab/private/CheckSymmtricLines.m
+++ matlab/private/CheckSymmtricLines.m
@@ -16,6 +16,19 @@ NP = numel(lines);
 range = lines(end)-lines(1);
 center = 0.5*(lines(end)+lines(1));
 
-if (abs(mean(lines)-center) < range*tolerance/NP)
-    result = true;
+% check all lines for symmetry
+for n=1:NP/2
+    if (abs((center-lines(n))-(lines(end-n+1)-center)) > range*tolerance/NP)
+        return;
+    end
 end
+
+% check central point to be symmetry-center
+if (mod(NP,2))
+    if (abs(lines((NP+1)/2)-center) > range*tolerance/NP)
+        return;
+    end
+end
+
+% if all checks pass, return true
+result = true;
