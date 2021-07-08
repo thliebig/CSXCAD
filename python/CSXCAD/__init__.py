048cd5344b06c68d44e86cfb42765c6ebf2cc741
Thorsten Liebig <liebig@imst.de>
2021-07-08 15:52:08 +0200

    python: option to set the openEMS install (dll) path on windows
    Signed-off-by: Thorsten Liebig <liebig@imst.de>

--- python/CSXCAD/__init__.py
+++ python/CSXCAD/__init__.py
@@ -4,4 +4,12 @@
 
 from __future__ import absolute_import
 
+import os
+if os.name == 'nt' and 'OPENEMS_INSTALL_PATH' in os.environ and os.path.exists(os.environ['OPENEMS_INSTALL_PATH']):
+    try:  # to use Python 3.8's DLL handling
+        os.add_dll_directory(os.environ['OPENEMS_INSTALL_PATH'])
+    except AttributeError:  # <3.8, use PATH
+        if os.environ['OPENEMS_INSTALL_PATH'] not in os.environ['PATH']:
+            os.environ['PATH'] += os.pathsep + os.environ['OPENEMS_INSTALL_PATH']
+
 from CSXCAD.CSXCAD import ContinuousStructure
