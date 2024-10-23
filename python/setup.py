c57344949883718d4b679b343f16d9d0d43eb3eb
Matias Senger <m.senger@hotmail.com>
2024-10-23 21:46:31 +0200

    Removed hardcoded path and improved error handling

--- python/setup.py
+++ python/setup.py
@@ -1,30 +1,33 @@
 from setuptools import Extension, setup
 from Cython.Build import cythonize
 from pathlib import Path
+import os
 
-# Hardcoded paths that would be better to remove but I don't know how to:
-PATH_TO_OPEN_EMS_INSTALLATION = Path(__file__).parent.parent.parent.parent
-
-# Define some functions to check that the packages are installed where we expect them to be installed, and raise meaningful errors otherwise:
-def check_that_folder_looks_like_openEMS_installation(path_to_some_folder:Path):
+def looks_like_openEMS_is_installed_there(path_to_some_folder:Path):
     path_to_some_folder = Path(path_to_some_folder)
     FILES_THAT_SHOULD_EXIST = [
         path_to_some_folder/'bin/openEMS',
         path_to_some_folder/'include/openEMS/openems.h',
     ]
     if any([not p.is_file() for p in FILES_THAT_SHOULD_EXIST]):
-        raise RuntimeError(f'I was expecting to find the installation of openEMS in {path_to_some_folder}, but it does not look like it is installed there. Please provide the path to where you have it installed and write it in `PATH_TO_OPEN_EMS_INSTALLATION` in the file {Path(__file__).resolve()}. Should you have it installed there, please create an issue in https://github.com/thliebig/openEMS reporting this. ')
+       return False
+    return True
 
-# Check that the packages are installed there:
-check_that_folder_looks_like_openEMS_installation(PATH_TO_OPEN_EMS_INSTALLATION)
+if 'OPENEMS_INSTALL_PATH' not in os.environ:
+    raise SystemExit('Please set the environment variable OPENEMS_INSTALL_PATH to point to where openEMS was installed. ')
+
+path_to_openEMS_installation = Path(os.environ['OPENEMS_INSTALL_PATH']) # Environment variables are always strings, so this should never raise any error.
+
+if not looks_like_openEMS_is_installed_there(path_to_openEMS_installation):
+    raise SystemExit(f'I was expecting to find openEMS installed in {path_to_openEMS_installation}, but it does not look like it is installed there. ')
 
 extensions = [
     Extension(
         '*', 
         ['CSXCAD/*.pyx'],
-        include_dirs = [str(Path(PATH_TO_OPEN_EMS_INSTALLATION)/'include')],
-        library_dirs = [str(Path(PATH_TO_OPEN_EMS_INSTALLATION)/'lib')],
-        runtime_library_dirs = [str(Path(PATH_TO_OPEN_EMS_INSTALLATION)/'lib')],
+        include_dirs = [str(path_to_openEMS_installation/'include')],
+        library_dirs = [str(path_to_openEMS_installation/'lib')],
+        runtime_library_dirs = [str(path_to_openEMS_installation/'lib')],
         language = 'c++',
         libraries = ['CSXCAD'],
     )
