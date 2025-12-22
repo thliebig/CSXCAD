2f98ef416722ae134afb28a93f8528bc3991ffdb
gadiLhv <gadi.lhv@gmail.com>
2025-12-22 23:00:38 +0200

    PR comment resolution

--- python/CSXCAD/CSProperties.pyx
+++ python/CSXCAD/CSProperties.pyx
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from hgext.keyword import kw_amend
 
 """
 Module for all Properties like metal, material, propes and dumps
