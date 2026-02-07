766af9b95b702bf695b867ab446b686e7212ae27
gadiLhv <gadi.lhv@gmail.com>
2026-02-07 18:19:01 +0200

    Initial Commit
    Still need to set the atributes or whateveer for SetExcitationFile.m

---
+++ matlab/SetExcitationFile.m
@@ -0,0 +1,41 @@
+function CSX = SetExcitationWeight(CSX, name, fileName)
+% function CSX = SetExcitationWeight(CSX, name, fileName)
+%
+% Define the mode file source of the excitation
+%
+% example:
+%
+%     start=[0 0 0];
+%     stop=[width height 0];
+%     CSX = AddExcitation(CSX,'excite',0,[1 1 0]);
+%     CSX = SetExcitationWeight(CSX,'excite','modeFile_E.csv');
+%     CSX = AddBox(CSX,'excite',0 ,start,stop);
+%
+% See also AddExcitation, InitCSX, DefineRectGrid
+%
+% CSXCAD matlab interface
+% -----------------------
+% Author: (2023-2025) Gadi Lahav - gadi@rfwithcare.com
+
+if ~isfield(CSX,'Properties')
+    error('CSXCAD::SetExcitationFile: no properties not found');
+end
+if ~isfield(CSX.Properties,'Excitation')
+    error('CSXCAD::SetExcitationFile: no excitation properties found');
+end
+
+pos=0;
+for n=1:numel(CSX.Properties.Excitation)
+   if  strcmp(CSX.Properties.Excitation{n}.ATTRIBUTE.Name, name)
+       pos=n;
+   end
+end
+
+if (pos==0)
+    error('CSXCAD::SetExcitationFile: property not found');
+    return;
+end
+
+CSX.Properties.Excitation{pos}.Weight.ATTRIBUTE.X = weight{1};
+CSX.Properties.Excitation{pos}.Weight.ATTRIBUTE.Y = weight{2};
+CSX.Properties.Excitation{pos}.Weight.ATTRIBUTE.Z = weight{3};
